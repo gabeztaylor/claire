2183017b9af7d6c22c9f8f8e00072344829039b6
Gabe Taylor <gabe.z.taylor@gmail.com>
2022-04-16 00:46:19 -0500

    emoji galore

--- dash_app.py
+++ dash_app.py
@@ -5,6 +5,7 @@ import dash_helpers as dh
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 from random import randint
+import re
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
@@ -18,6 +19,7 @@ df['Day'] = df['Message Date'].dt.date
 df['Time'] = df['Message Date'].dt.time
 df['Hour'] = df['Message Date'].dt.hour
 df['Type'] = df['Type'].apply(lambda x: 'Claire' if x == 'Incoming' else 'Gabe')
+df.Text = df.Text.apply(lambda x: re.sub('“.*?”', '', x) if not pd.isnull(x) else x)
 
 
 ##########################
@@ -27,6 +29,20 @@ df['Type'] = df['Type'].apply(lambda x: 'Claire' if x == 'Incoming' else 'Gabe')
 day_ts_fig = dh.plot_by_day(df)
 hour_ts_fig = dh.plot_by_hour(df)
 word_tbl = dh.words_table(df)
+txt_dist = dh.text_length(df)
+#############################
+
+#############################
+# Stats #####################
+#############################
+n_txts = len(df)
+wrd_hnt = dh.n_games(df)
+emots_fig, emots_df = dh.emoji_cnt(df)
+print(emots_df)
+
+
+#############################
+# Data Tables ###############
 #############################
 
 corpus_datatable = dash_table.DataTable(
@@ -52,22 +68,55 @@ corpus_datatable = dash_table.DataTable(
     page_size=10
 )
 
+emots_dt = dash_table.DataTable(
+    id='claire-emot-table',
+    data=emots_df.to_dict('records'),
+    columns = [{'id': c, 'name': c} for c in emots_df.columns],
+    style_table={
+        'overflowY': 'scroll',
+        'overflowX': 'scroll',
+        'float' : 'right'
+        },
+    style_cell={
+        'overflow': 'hidden',
+        'textOverflow': 'ellipsis',
+        'maxWidth': 0
+    },
+    tooltip_data=[
+        {column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in emots_df.to_dict('records')
+    ],
+    tooltip_duration=None,
+    filter_action="native",
+    sort_action="native",
+    page_size=10
+)
+
+
+
 app.layout = html.Div(children=[
     html.H1(children='Happy 1 Year* of dating!', style = {'text-align' : 'center'}),
+    
+    html.H3(children=f'{n_txts:,} Texts! {wrd_hnt:,} word hunt games! and lots of love ❤️', style = {'text-align' : 'center'}),
 
     html.Div(children='*(2 months and one week)', style = {'text-align' : 'center'}),
     
     html.Div(children=[
         dcc.Graph(figure = day_ts_fig, id="graph1", style = {'display' : 'inline-block'}),
         dcc.Graph(figure = hour_ts_fig, id="graph2", style = {'display' : 'inline-block'}),
-        html.Div([corpus_datatable]),
+        dcc.Graph(figure = txt_dist, id="graph3", style = {'display' : 'inline-block'}),
         dcc.Interval(
             id='interval-component',
-            interval=10000, # in milliseconds
+            interval=30000, # in milliseconds
             n_intervals=0)
     ], style = {'display' : 'flex'}),
+    
+    html.Div(children=[
     html.P(id = 'random-text', style={'background-image': 'url(/assets/txt_bubble.png)',
-                                      'width': '500px', 'height' : '350px', 'text-align' : 'center', 'vertical-align' : 'center'})
+                                      'width': '500px', 'height' : '350px', 'text-align' : 'center', 'vertical-align' : 'center'}),
+    html.Div([corpus_datatable]),
+    html.Div([emots_dt]),
+    dcc.Graph(figure = emots_fig, id="graph4", style = {'display' : 'inline-block'}),
+    ], style = {'display' : 'flex'})
 ])
 
 
@@ -78,7 +127,7 @@ app.layout = html.Div(children=[
 def sample_text(n):
     randi = randint(0, len(df))
     
-    text = df.iloc[randi:(randi+5), : ]
+    text = df.iloc[randi:(randi+10), : ]
     date = text.Day.iloc[0]
     final = [f'Date: {date}', html.Br()]
     
--- dash_helpers.py
+++ dash_helpers.py
@@ -3,6 +3,9 @@ import numpy as np
 from collections import Counter
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import emoji
+import regex
 
 def plot_by_day(df):
     plot_df = df.groupby(['Day', 'Type'])\
@@ -39,4 +42,69 @@ def words_table(df):
     cnts.columns = ['person', 'word', 'count']
     
     return cnts.sort_values('count', ascending = False)
+
+def text_length(df):
+    plot_df = df.copy()
+    plot_df['length'] = plot_df.Text.fillna('').apply(lambda x: x.split()).apply(len)
+    plot_df = plot_df.loc[plot_df.Type != 'Notification', :].reset_index()
+    plot_df = plot_df.groupby(['Type', 'length']).apply(lambda x: len(x)).reset_index().rename(columns = {0: 'count'})
+
+    fig = px.bar(plot_df, x="length", y="count",
+             color="Type", barmode = 'group')
+    
+    fig.update_layout(xaxis_range=[-2,20])
+    
+    return fig
+
+def n_games(df):
+    return df.Text.fillna('').apply(lambda x: 'word hunt' in ' '.join([i.lower() for i in x.split()])).sum()
+
+##########
+# Emojis #
+##########
+
+def split_count(text):
+
+    emoji_list = []
+    data = regex.findall(r'\X', text)
+    for word in data:
+        if any(char in emoji.UNICODE_EMOJI['en'] for char in word):
+            emoji_list.append(word)
+    
+    return emoji_list
+
+def emoji_cnt(df):
+    claire_corpus = df.loc[df.Type == 'Claire', 'Text'].str.cat(sep=' ')
+    claire_corpus = [i.lower() for i in claire_corpus.split()]
+    
+    gabe_corpus = df.loc[df.Type == 'Gabe', 'Text'].str.cat(sep=' ')
+    gabe_corpus = [i.lower() for i in gabe_corpus.split()]
+    
+    stop_emots = ['🟨', '🟩', '⬛', '', '  ']
+    
+    ### Claire
+    emojies = split_count(' '.join(claire_corpus))
+    claire_emojis = ' '.join(emoji for emoji in emojies if emoji not in stop_emots)
+    claire_emojis = pd.DataFrame(dict(Counter(claire_emojis.split()).most_common()), index = range(1)).transpose().reset_index()
+    claire_emojis = claire_emojis.rename(columns = {'index' : 'emoji', 0 : 'n'})
+    claire_emojis['person'] = 'Claire'
+    
+    ### Gabe
+    emojies = split_count(' '.join(gabe_corpus))
+    gabe_emojis = ' '.join(emoji for emoji in emojies if emoji not in stop_emots)
+    
+    gabe_emojis = pd.DataFrame(dict(Counter(gabe_emojis.split()).most_common()), index = range(1)).transpose().reset_index()
+    gabe_emojis = gabe_emojis.rename(columns = {'index' : 'emoji', 0 : 'n'})
+    gabe_emojis['person'] = 'Gabe'
+    
+    emot_df = claire_emojis.append(gabe_emojis)
+    
+    plot_df = claire_emojis.sort_values(by = 'n', ascending = False).iloc[:10, :]\
+                           .append(gabe_emojis.sort_values(by = 'n', ascending = False).iloc[:10, :])
+    
+    fig = px.bar(plot_df, x="emoji", y="n", color="person", barmode = 'group')
+    
+    return fig, emot_df
+    
+    
     
