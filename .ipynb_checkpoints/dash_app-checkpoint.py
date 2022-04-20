37d302d98d85beafc68133665a5fd5bafe47e0ba
Gabe Taylor <gabe.z.taylor@gmail.com>
2022-04-19 20:52:21 -0500

    add pics

--- .ipynb_checkpoints/dash_app-checkpoint.py
+++ .ipynb_checkpoints/dash_app-checkpoint.py
@@ -7,6 +7,9 @@ from dash.dependencies import Input, Output, State
 from random import randint
 import re
 import dash_daq as daq
+import os
+import base64
+
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 
@@ -14,13 +17,20 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
 # Read in Data and Clean #
 ##########################
 
-df = pd.read_csv('/Users/gabrieltaylor/Python/Claire/Messages - Claire Robinson.csv')
+df = pd.read_csv('Messages - Claire Robinson.csv')
 df['Message Date'] = df['Message Date'].apply(pd.to_datetime)
 df['Day'] = df['Message Date'].dt.date
 df['Time'] = df['Message Date'].dt.time
 df['Hour'] = df['Message Date'].dt.hour
 df['Type'] = df['Type'].apply(lambda x: 'Claire' if x == 'Incoming' else 'Gabe')
 df.Text = df.Text.apply(lambda x: re.sub('“.*?”', '', x) if not pd.isnull(x) else x)
+
+##########################
+# Pics ###################
+##########################
+
+pictures = os.listdir('assets')
+pictures.remove('.ipynb_checkpoints')
 
 
 ##########################
@@ -46,7 +56,7 @@ qck_stats = dh.get_stats(df)
 # Data Tables ###############
 #############################
 
-stats_table = dbc.Table.from_dataframe(qck_stats, striped=True, bordered=True, hover=True, index=False,
+stats_table = dbc.Table.from_dataframe(qck_stats, striped=True, bordered=True, hover=True, index=False, size = 'sm',
                                       style = {'font-size' : '20px', 'text-align' : 'center'})
 
 corpus_datatable = dash_table.DataTable(
@@ -106,9 +116,24 @@ app.layout = html.Div(children=[
     
     html.Br(),
     
-    html.H3('Some Quick Stats', style = {'text-align' : 'center'}),
+    html.Br(),
     
-    html.Div([stats_table], style = {'width': '30%', 'marginLeft' : 'auto', 'marginRight' : 'auto'}),
+    html.Div([dbc.Col([html.P(id = 'random-text', style={'vertical-align' : 'center', 'display' : 'inline-block', "margin-left": "50px"})]),
+              dcc.Interval(id='interval-component1', interval=20000, n_intervals=0),
+              
+              dbc.Col([html.H3('Some Quick Stats', style = {'text-align' : 'center'}),
+                      html.Div([stats_table])]),
+              
+              dbc.Col([html.H3('How do we look?', style = {'text-align' : 'center'}),
+                       html.Div([html.Img(id='random-pic')],  style = {'display': 'block', 'marginLeft': 'auto' ,
+                                                                       'marginRight': 'auto', 'width' : '50%'})]),
+              dcc.Interval(id='interval-component2', interval=20000, n_intervals=0)
+             ], style = {'display' : 'flex'}),
+    
+    html.Br(),
+    
+    html.Br(),
+    
     
     html.Div([
         dbc.Col([html.H3('Our Total Texts per Day'),
@@ -153,23 +178,35 @@ app.layout = html.Div(children=[
 ])
 
 
-# @app.callback(
-#     Output('random-text', 'children'),
-#     Input('interval-component', 'n_intervals')
-# )
-# def sample_text(n):
-#     randi = randint(0, len(df))
+@app.callback(
+    Output('random-text', 'children'),
+    Input('interval-component1', 'n_intervals')
+)
+def sample_text(n):
+    randi = randint(0, len(df) - 1)
     
-#     text = df.iloc[randi:(randi+10), : ]
-#     date = text.Day.iloc[0]
-#     final = [f'Date: {date}', html.Br()]
+    text = df.iloc[randi:(randi+10), : ]
+    date = text.Day.iloc[0]
+    final = [html.H3(f'What did we say on {date}?', style = {'text-align' : 'center'}), html.Br()]
     
-#     for i in text.index:
-#         txt = text.loc[i, 'Text']
-#         sender = text.loc[i, 'Type']
-#         final += [f"{sender}: {txt}", html.Br()]
+    for i in text.index:
+        txt = text.loc[i, 'Text']
+        sender = text.loc[i, 'Type']
+        final += [html.Strong(f"{sender}: "), html.Span(f"{txt}"), html.Br()]
         
-#     return final
+    return final
+
+@app.callback(
+    Output('random-pic', 'src'),
+    Input('interval-component1', 'n_intervals'))
+def update_image_src(n):
+    # print the image_path to confirm the selection is as expected
+    randi = randint(0, len(pictures) - 1)
+    
+    image_path = f"assets/{pictures[randi]}"
+    print('current image_path = {}'.format(image_path))
+    encoded_image = base64.b64encode(open(image_path, 'rb').read())
+    return 'data:image/png;base64,{}'.format(encoded_image.decode())
 
 
 @app.callback(
@@ -194,4 +231,4 @@ def choose_ngram(n, stops):
     return fig
 
 app.run_server(debug=True)
-
+# app.run_server(host='0.0.0.0', port=8050)
